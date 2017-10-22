d8191965bed650a8a5992e3b99b21ae911ea7488
Paul T <paultavares1@gmail.com>
2017-10-22 16:01:33 -0400

    Bug fix: on directive: pass correct context to direction value getter

--- src/directives/on-directive.js
+++ src/directives/on-directive.js
@@ -1,5 +1,6 @@
-import domAddEventListener from "common-micro-libs/src/domutils/domAddEventListener"
-import Directive           from "./Directive"
+import domAddEventListener  from "common-micro-libs/src/domutils/domAddEventListener"
+import { observableAssign } from "observable-data/src/ObservableObject"
+import Directive            from "./Directive"
 import {
     PRIVATE,
     escapeString,
@@ -14,27 +15,35 @@ const matchesDirective      = new RegExp(`^${ escapeString(DIRECTIVE) }(.*)`);
 
 const OnDirective = Directive.extend({
     init(ele, directiveAttr) {
-        let dataForTokenValueGetter = {};
+        let dataForTokenValueGetter = { $data: {} };
         let tokenValueGetter        = createValueGetter(getAttribute(ele, directiveAttr));
         const eventName         = (new RegExp(matchesDirective)).exec(directiveAttr)[1];
         const eventHandler      = domEv => {
             let tokenValue;
+            dataForTokenValueGetter.$ev = domEv;
 
             try {
-                tokenValue = tokenValueGetter({ $data: dataForTokenValueGetter, $ev: domEv });
+                tokenValue = tokenValueGetter(dataForTokenValueGetter);
             }
             catch(e) {
                 console.error(e);
                 return;
             }
 
+            dataForTokenValueGetter.$ev = null;
+
             if ("function" === typeof tokenValue) {
                 tokenValue(domEv);
             }
         };
-        const updater           = data => {
+        const updater = data => {
             if (data) {
-                dataForTokenValueGetter = data;
+                if (data.$data) {
+                    observableAssign(dataForTokenValueGetter, data);
+                }
+                else {
+                    observableAssign(dataForTokenValueGetter.$data, data);
+                }
             }
         };
         const inst = { updater };
